b9b413730af136b60ea04af1ee0c7df8ca053c00
Hamed Fakoori <hamedfakoori78@gmail.com>
2023-11-06 19:54:11 +0330

    create base templates and use it in home url

--- configs/urls.py
+++ configs/urls.py
@@ -4,8 +4,12 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static 
 from django.conf import settings
 
+# local 
+from my_site.views import Home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home , name="home"),
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL ,document_root=settings.MEDIA_ROOT )
--- my_site/views.py
+++ my_site/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def Home(request) :
+    return render(request , 'home/index.html')
