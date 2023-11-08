974f508196eca415eb2b93e0b82764bb7b9ec504
Hamed Fakoori <hamedfakoori78@gmail.com>
2023-11-08 17:07:23 +0330

    create base api : views , urls and serializers for test

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from scraper.serializers import ReconSiteSerializer
 
-# Create your views here.
+
+class ReconSiteView (APIView):
+    def post(self , request):
+        permission_classes = [IsAuthenticated]
+        serializer = ReconSiteSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data , status=200)
+        return Response(serializer.errors , status=403)
