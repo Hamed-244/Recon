f478be8bd3d580a3bbb403e43add71c81ce4f1ce
Hamed Fakoori <hamedfakoori78@gmail.com>
2023-11-08 17:06:19 +0330

    configur rest framework default renderer class

--- configs/settings.py
+++ configs/settings.py
@@ -142,3 +142,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# local
+
+# rest framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
