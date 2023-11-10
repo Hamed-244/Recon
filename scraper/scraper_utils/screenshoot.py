9ec2596caa90f348ecc1fe766cfec7180304071a
Hamed Fakoori <hamedfakoori78@gmail.com>
2023-11-10 16:37:04 +0330

    fix screenshoot.py file to return default screenshoot if it was None

--- scraper/scraper_utils/screenshoot.py
+++ scraper/scraper_utils/screenshoot.py
@@ -14,7 +14,7 @@ def capture_screenshot(url , domain ):
         driver.get(url)
         time.sleep(3)
         # Create the screenshots directory if it doesn't exist
-        screenshots_dir = os.path.join(settings.MEDIA_ROOT, 'screenshots')
+        screenshots_dir = os.path.join(settings.MEDIA_ROOT, 'screenshoots')
         if not os.path.exists(screenshots_dir):
             os.makedirs(screenshots_dir)
 
@@ -24,10 +24,11 @@ def capture_screenshot(url , domain ):
 
         # Clean up
         driver.quit()
-
+        dfs()
         # Return the URL of the screenshot
         return screenshot_path
     except Exception as error:
         # Handle any exceptions
         print('error in capture screenshoot' , error)
-        return None
+        default_screenshoot_dir = os.path.join(settings.MEDIA_ROOT, 'screenshoots/default.png')
+        return default_screenshoot_dir
