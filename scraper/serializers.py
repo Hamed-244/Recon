79233718b353f28345042f4aa85c6e2b882f9028
Hamed Fakoori <hamedfakoori78@gmail.com>
2023-11-12 19:23:49 +0330

    fix serializer known bugs

--- scraper/serializers.py
+++ scraper/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 import requests
-
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 
 class ReconSiteSerializer(serializers.Serializer) :
     url = serializers.CharField(max_length=512)
 
     def validate_url(self , value):
-        try :
-            check_url = requests.get(value if value.startswith('http') else f'http://{value}')
-            if check_url.status_code == 200 :
-                return value
+
+        main_url = value if value.startswith('http') else f'http://{value}'
+        try:
+            request = requests.get(main_url)
+            return main_url
+        except Exception as e:
             raise serializers.ValidationError('Can not access to this url')
-        except Exception as error:
-            print(error)
-            raise serializers.ValidationError('Invalid url please enter a valid url')
