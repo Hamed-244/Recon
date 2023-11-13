a81187283e7b5e2116523ebf077f5fba9d923d98
Hamed Fakoori <hamedfakoori78@gmail.com>
2023-11-13 19:28:09 +0330

    fix coding style in open_ports.py file

--- scraper/scraper_utils/open_ports.py
+++ scraper/scraper_utils/open_ports.py
@@ -3,13 +3,13 @@ import socket
 def port_scanner (domain) :
     try :
         ports = []
-        ipAddres = socket.gethostbyname(domain)
+        ip_addres = socket.gethostbyname(domain)
         common_ports = [21, 22, 23, 25, 53, 80, 110, 119, 123, 143, 161, 194, 443, 445, 993, 995]
 
         for port in common_ports:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(1)
-            result = sock.connect_ex((ipAddres, port))
+            result = sock.connect_ex((ip_addres, port))
             sock.close()
             if result == 0:
                 ports.append(port)
