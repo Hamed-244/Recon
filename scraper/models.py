a473ccc2a86ec6a2bef4d59ea5bc84fb8e2bdf9e
Hamed Fakoori <hamedfakoori78@gmail.com>
2023-11-16 18:50:37 +0330

    feat: create screenshoot model

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Screenshoot(models.Model) :
+    url = models.CharField(max_length=512)
+    domain = models.CharField(max_length=128)
+    screenshoot = models.ImageField(verbose_name='Screenshoot' , default = 'screenshoots/default.png' , upload_to='screenshoots/')
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.domain
